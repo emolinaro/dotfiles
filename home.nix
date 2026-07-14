787680ee4730fcd1a126629acffc150ac481b6cc
Emiliano Molinaro <40191802+emolinaro@users.noreply.github.com>
2026-07-14 22:00:16 +0200

    Remove commented-out Starship configuration from home.nix to clean up the file.

--- home.nix
+++ home.nix
@@ -301,20 +301,6 @@ in
     };
   };
 
-
-#  programs.starship = {
-#    enable = true;
-#    settings = {
-#      add_newline = false;
-#      format = "$directory$git_branch$git_status$cmd_duration$line_break$character";
-#      character = {
-#        success_symbol = "[❯](purple)";
-#        error_symbol = "[❯](red)";
-#      };
-#      cmd_duration.format = "[$duration]($style) ";
-#    };
-#  };
-
   # Edit-in-place: the real file stays in my repo, ~/.config just points at it.
   home.file.".config/nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/nvim";
