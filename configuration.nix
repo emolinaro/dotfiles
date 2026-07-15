2a2994889d7a09e4c1565613a802b1027bb231e3
Emiliano Molinaro <emil.molinaro@gmail.com>
2026-07-15 13:19:29 +0200

    Enhance dynamic user resolution for macOS setup by automatically detecting the current user and home directory. Update README to reflect changes and remove obsolete design document. Adjust bootstrap and rebuild scripts to support impure evaluation for improved flexibility.

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ homeDirectory, username, ... }:
 
 {
   # Determinate already manages the Nix daemon, so nix-darwin shouldn't.
@@ -7,9 +7,9 @@
   nixpkgs.config.allowUnfree = true;
   nixpkgs.hostPlatform = "aarch64-darwin"; # use x86_64-darwin for Intel CPU
 
-  system.primaryUser = "molinaro";
-  users.users.molinaro = {
-    home = "/Users/molinaro";
+  system.primaryUser = username;
+  users.users.${username} = {
+    home = homeDirectory;
   };
   system.stateVersion = 6;
   system.defaults = {
@@ -27,7 +27,7 @@
   };
   nix-homebrew = {
     enable = true;
-    user = "molinaro";
+    user = username;
   };
   homebrew = {
     enable = true;
